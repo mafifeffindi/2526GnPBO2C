118e73f54129b44f29f14fa4e195a315f15e657b
adhimatur rizqiyah <adhimaturria@gmail.com>
2026-06-04 13:49:17 +0700

    Create Mahasiswa.java

---
+++ Pekan 13/055_Adhimatur Rizqiyah/Mahasiswa.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+class Mahasiswa {
+
+    // Atribut
+    String nama;
+    String nim;
+    String jurusan;
+
+    // Constructor
+    Mahasiswa(String nama, String nim, String jurusan) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+    }
+
+    // Method untuk menampilkan data
+    void tampilData() {
+        System.out.println("Nama     : " + nama);
+        System.out.println("NIM      : " + nim);
+        System.out.println("Jurusan  : " + jurusan);
+        System.out.println();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        // Membuat object pertama
+        Mahasiswa mhs1 = new Mahasiswa(
+                "Adhimatur Rizqiyah",
+                "230055",
+                "Pendidikan Informatika"
+        );
+
+        // Membuat object kedua
+        Mahasiswa mhs2 = new Mahasiswa(
+                "Rizqiyah",
+                "230056",
+                "Sistem Informasi"
+        );
+
+        // Memanggil method tampilData()
+        mhs1.tampilData();
+        mhs2.tampilData();
+    }
+}
